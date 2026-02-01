55d81bf6d82ad2e8ab7d6312daa96ccb5d7edd0a
HarryL0L <88073458+HarryL0L@users.noreply.github.com>
2026-02-01 17:32:30 +0530

    kmime

---
+++ x11-packages/kmime/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/pim/kmime"
+TERMUX_PKG_DESCRIPTION="Library for handling mail messages and newsgroup articles"
+TERMUX_PKG_LICENSE="LGPL-2.0-or-laterr"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="25.12.1"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/release-service/${TERMUX_PKG_VERSION}/src/kmime-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="c87528fd679cd27c64bb3ff0b34f4f7d3518e3fc3c9e9740ad01a147fe03c479"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="kf6-kcodecs, libc++, qt6-qtbase"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
