da25c7236ee5d8962d0ef56f7c0ab02bda679963
HarryL0L <88073458+HarryL0L@users.noreply.github.com>
2026-02-01 22:40:07 +0530

    Update build.sh

--- packages/libaccounts-glib/build.sh
+++ packages/libaccounts-glib/build.sh
@@ -2,10 +2,12 @@ TERMUX_PKG_HOMEPAGE="https://gitlab.com/accounts-sso/libaccounts-glib"
 TERMUX_PKG_DESCRIPTION="Glib-based client library for accessing the online accounts database"
 TERMUX_PKG_LICENSE="LGPL-2.1-or-later"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="VERSION_1.27"
+TERMUX_PKG_VERSION="1.27"
 TERMUX_PKG_SRCURL="https://gitlab.com/accounts-sso/libaccounts-glib/-/archive/${TERMUX_PKG_VERSION}/libaccounts-glib-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256="a8407a5897a2e425ea1aa955ecf88485dd2fd417919de275b27c781a5d0637a5"
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_REGEXP='^VERSION_[0-9.]+$'
+TERMUX_PKG_UPDATE_VERSION_SED_REGEXP='s/^VERSION_//'
 TERMUX_PKG_DEPENDS="glib, libc++, libxml2, sqlite"
 TERMUX_PKG_BUILD_DEPENDS="check, g-ir-scanner, glib, gobject-introspection, valac"
 
