b61f9f0ead67871b0040689c8c494d5cb6f70bbf
HarryL0L <88073458+HarryL0L@users.noreply.github.com>
2026-02-02 20:51:10 +0530

    signond

--- x11-packages/signond/build.sh
+++ x11-packages/signond/build.sh
@@ -1,6 +1,7 @@
 TERMUX_PKG_HOMEPAGE="https://gitlab.com/accounts-sso/signond/"
 TERMUX_PKG_DESCRIPTION="A D-Bus service which performs user authentication on behalf of its clients"
 TERMUX_PKG_LICENSE="LGPL"
+TERMUX_PKG_LICENSE_FILE="COPYING"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="8.61"
 TERMUX_PKG_SRCURL="https://gitlab.com/accounts-sso/libaccounts-qt/-/archive/VERSION_${TERMUX_PKG_VERSION}/signond-VERSION_${TERMUX_PKG_VERSION}.tar.gz"
