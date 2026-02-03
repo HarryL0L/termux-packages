bcc36dcfbff07e4dd55dd55a45e654f3d4889b67
HarryL0L <88073458+HarryL0L@users.noreply.github.com>
2026-02-03 21:26:17 +0530

    kaccounts-integration

---
+++ x11-packages/kaccounts-integration/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/network/kaccounts-integration"
+TERMUX_PKG_DESCRIPTION="Online account management system and its Plasma integration components"
+TERMUX_PKG_LICENSE="GPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="25.12.1"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/release-service/${TERMUX_PKG_VERSION}/src/kaccounts-integration-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="a449426e849b0562622585d11a12f8b407bbf9cd3a988083b68f17a657727b45"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="kf6-kcmutils, kf6-kconfig, kf6-kcoreaddons, kf6-kdbusaddons, kf6-ki18n, kf6-kio, kf6-kirigami, kf6-kwallet, libaccounts-qt, libc++, qt6-qtbase, qt6-qtdeclarative, signon-kwallet-extension, signon-plugin-oauth2, signon-ui, signond"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qcoro, kf6-kdoctools"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
