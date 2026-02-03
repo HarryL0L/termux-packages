17c7ac4ee8792aac5b7da4e160517760076c278a
HarryL0L <88073458+HarryL0L@users.noreply.github.com>
2026-02-03 22:12:52 +0530

    kaccounts-integration

--- x11-packages/kaccounts-integration/build.sh
+++ x11-packages/kaccounts-integration/build.sh
@@ -7,9 +7,15 @@ TERMUX_PKG_SRCURL="https://download.kde.org/stable/release-service/${TERMUX_PKG_
 TERMUX_PKG_SHA256="a449426e849b0562622585d11a12f8b407bbf9cd3a988083b68f17a657727b45"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="kf6-kcmutils, kf6-kconfig, kf6-kcoreaddons, kf6-kdbusaddons, kf6-ki18n, kf6-kio, kf6-kirigami, kf6-kwallet, libaccounts-qt, libc++, qt6-qtbase, qt6-qtdeclarative, signon-kwallet-extension, signon-plugin-oauth2, signon-ui, signond"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qcoro, kf6-kdoctools"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qcoro, qcoro-static, kf6-kdoctools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_SYSTEM_NAME=Linux
 -DKDE_INSTALL_QMLDIR=lib/qt6/qml
 -DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
 "
+
+termux_step_pre_configure() {
+	if [[ "$TERMUX_ON_DEVICE_BUILD" == "false" ]]; then
+		TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -DKF6_HOST_TOOLING=$TERMUX_PREFIX/opt/kf6/cross/lib/cmake/"
+	fi
+}
