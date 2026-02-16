07a4dd06b89c80a20d428107ec551b7e8eb2691a
HarryL0L <88073458+HarryL0L@users.noreply.github.com>
2026-02-16 11:21:50 +0530

    Create build.sh

---
+++ x11-packages/libudev-zero/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/illiliti/libudev-zero"
+TERMUX_PKG_DESCRIPTION="libudev replacement to use whatever device manager"
+TERMUX_PKG_LICENSE="ISC"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="1.0.3"
+TERMUX_PKG_SRCURL="https://github.com/illiliti/libudev-zero/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256="0bd89b657d62d019598e6c7ed726ff8fed80e8ba092a83b484d66afb80b77da5"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++"
