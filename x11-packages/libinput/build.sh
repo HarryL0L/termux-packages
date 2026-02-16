10dc0f0f895a1d42324f7e66e0de16100077682a
HarryL0L <88073458+HarryL0L@users.noreply.github.com>
2026-02-16 15:30:54 +0530

    libinput

--- x11-packages/libinput/build.sh
+++ x11-packages/libinput/build.sh
@@ -5,7 +5,6 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="1.31.0"
 TERMUX_PKG_SRCURL="https://gitlab.freedesktop.org/libinput/libinput/-/archive/${TERMUX_PKG_VERSION}/libinput-${TERMUX_PKG_VERSION}.tar.gz"
 TERMUX_PKG_SHA256="5f95a8ce039b3b2b4560a60bbdb920518d41baa9419344e5878c94bd841d5a54"
-TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, libevdev, lua52, libudev-zero, mtdev"
 TERMUX_PKG_BUILD_DEPENDS="check, gtk4, libudev-zero, libwayland, libwayland-protocols, libx11, python"
