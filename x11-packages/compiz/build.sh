90dec624c4c91681d4e75faa4d9d8753baa820d2
HarryL0L <88073458+HarryL0L@users.noreply.github.com>
2026-03-19 23:30:33 +0530

    Update build.sh

--- x11-packages/compiz/build.sh
+++ x11-packages/compiz/build.sh
@@ -5,7 +5,7 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.9.14.2
 TERMUX_PKG_SRCURL="https://launchpad.net/compiz/${TERMUX_PKG_VERSION%.*}/${TERMUX_PKG_VERSION}/+download/compiz-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=cfa061e93b032275ff9e7041f582a8f6d5ae271cf8a89e6bc74e3d3635999d3c
-TERMUX_PKG_DEPENDS="dbus-python, glu, libglibmm-2.4, libice, libnotify, libsm, libwnck, protobuf, pycairo, pygobject, python"
+TERMUX_PKG_DEPENDS="dbus-python, glu, libglibmm-2.4, libice, libnotify, libsm, libwnck, libxslt, protobuf, pycairo, pygobject, python"
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE=newest-tag
 
