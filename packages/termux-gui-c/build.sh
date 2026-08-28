b0d95dd2b1e223e8ef05835d0a037e05c10da179
HarryL0L <88073458+HarryL0L@users.noreply.github.com>
2026-08-28 20:56:34 +0530

    test

--- packages/termux-gui-c/build.sh
+++ packages/termux-gui-c/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE="https://github.com/tareksander/termux-gui-c-bindings"
 TERMUX_PKG_DESCRIPTION="A C library for the Termux:GUI plugin"
 TERMUX_PKG_LICENSE="MPL-2.0"
 TERMUX_PKG_MAINTAINER="@tareksander"
-TERMUX_PKG_VERSION="0.1.3"
+TERMUX_PKG_VERSION="0.1"
 TERMUX_PKG_REVISION=7
 TERMUX_PKG_DEPENDS="abseil-cpp, libc++, libandroid-stub, libprotobuf"
-TERMUX_PKG_SRCURL="https://github.com/tareksander/termux-gui-c-bindings/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
-TERMUX_PKG_SHA256=7781fdbd37ca1376b4c2339440976b0aa00cc2188592ea51438e473589db466f
+TERMUX_PKG_SRCURL="https://github.com/HarryL0L/termux-gui-c-bindings/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256=e0095c6dcc0b77dba11a6d033ae3815f98024cc4d6261e4a8b368c5b7653b9
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
 
